a2a5b171b9404067dc05daf837e4c7dc3411748e
Samuel Gfrörer <SamuelGfroerer@googlemail.com>
2014-02-25 12:31:52 +0100

    experimenting with type class hierarchies (not compiling)

---
+++ SGData/Indexable/Card2.hs
@@ -0,0 +1,90 @@
+{-# LANGUAGE MultiParamTypeClasses, FlexibleInstances, FlexibleContexts, UndecidableInstances, ScopedTypeVariables, Rank2Types #-}
+module Card2 where
+
+import Data.Reflection
+import Data.Proxy
+
+data Zero = Zero
+data Succ a = Succ a
+
+type N0 = Zero
+type N1 = Succ N0
+type N2 = Succ N1
+type N3 = Succ N2
+type N4 = Succ N3
+type N5 = Succ N4
+type N6 = Succ N5
+
+n0 = Zero
+n1 = Succ n0
+n2 = Succ n1
+n3 = Succ n2
+n4 = Succ n3
+n5 = Succ n4
+n6 = Succ n5
+
+instance Card Zero where fromCard _ = 0
+instance (Card n) => Card (Succ n) where fromCard _ = fromCard (undefined :: n) + 1
+instance Show Zero where
+	show = show . fromCard
+instance (Card n) => Show (Succ n) where
+	show = show . fromCard
+
+class LE l r
+instance LE Zero Zero
+instance (LE l r) => LE (Succ l) (Succ r)
+instance (LE l r) => LE l (Succ r)
+
+class LT l r 
+instance LT Zero N1
+instance (LT l r) => LT (Succ l) (Succ r)
+instance (LT l r) => LT l (Succ r)
+
+data CardProx config = CardProx
+
+class Card c where
+	fromCard :: c -> Int
+
+class Card2 c where
+	fromCard2 :: c -> (Int,Int)
+class Card3 c where
+	fromCard3 :: c -> (Int,Int,Int)
+{-
+class (Card size) => CardList size l where
+	getCard :: (LT index size, Card n) => index -> l -> n
+class CardN c n | c -> n where
+	fromCardN :: (Card index, Card res) => index -> c -> res
+-}
+
+-- data Proxy config = Proxy
+instance (Reifies config Int) => Card (Proxy config) where
+	fromCard x = (reflect x)
+instance (Reifies config (Int,Int)) => Card2 (Proxy config) where
+	fromCard2 x = (reflect x)
+instance (Reifies config (Int,Int,Int)) => Card3 (Proxy config) where
+	fromCard3 x = (reflect x)
+
+{-
+withCard :: Int -> (forall n. Card n => n -> w) -> w
+withCard i f = withInt f'
+	where
+		f' x = 
+-}
+
+withInt :: Int -> (forall n . Reifies n Int => Proxy n -> w) -> w
+withInt i f = reify i f
+
+withInt2 :: (Int,Int) -> (forall n . Reifies n (Int,Int) => Proxy n -> w) -> w
+withInt2 ii f = reify ii f
+
+withInt3 :: (Int,Int,Int) -> (forall n . Reifies n (Int,Int,Int) => Proxy n -> w) -> w
+withInt3 i f = reify i f
+
+test = withInt 10 (show . fromCard)
+test2 = withInt2 (1,2) $ show . fromCard2
+
+{-
+data CardInstance = forall config . Card config => CardInstance config
+instance Card CardInstance where
+	toInt (CardInstance config) = toInt config
+-}
--- SGData/Indexable/Classes.hs
+++ SGData/Indexable/Classes.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE MultiParamTypeClasses, FunctionalDependencies, FlexibleInstances, FlexibleContexts, UndecidableInstances, ScopedTypeVariables #-}
+{-# LANGUAGE MultiParamTypeClasses, FunctionalDependencies, FlexibleInstances, FlexibleContexts, ScopedTypeVariables #-}
 module SGData.Indexable.Classes(
 	Listable,
 	ListableCT,
@@ -6,39 +6,106 @@ module SGData.Indexable.Classes(
 	fromList
 ) where
 
-import SGCard
+--import SGCard
+import Card2
+--import SGData.Indexable.Card2
 import Data.Array
+import Data.Reflection
+import Data.Proxy
+import Data.Maybe(fromJust)
 
-
-class LE l r
-instance LE Zero Zero
-instance (LE l r) => LE (Succ l) (Succ r)
-instance (LE l r) => LE l (Succ r)
-
-class LT l r 
-instance LT Zero N1
-instance (LT l r) => LT (Succ l) (Succ r)
-instance (LT l r) => LT l (Succ r)
-
-class (Card size) => CardList size l where
-	getCard :: (LT index size, Card n) => index -> l -> n
-
-class (Ix i) => Indexable l i a | l -> i, l -> a where
-	get :: i -> l -> a
 
 {-
-class (Indexable l i a, Ix i) => FixedSizeIndexable l i a | l -> i, l -> a where
-	bounds :: l -> (i,i)
+class (Ix i) => Indexable l i a | l -> i, l -> a where
+	get :: i -> l -> a
 -}
 
+-- up bounded: 
 class UpBounded l i | l -> i where
 	upperBound :: l -> i
 
 class (Card size) => UpBoundedCT l size i | l -> size, l -> i where
 
-instance (Card size, UpBoundedCT l size Int) => UpBounded l Int where
-	upperBound _ = toInt (undefined :: size)
+-- down bounded: 
+class DownBounded l i | l -> i where
+	lowerBound :: l -> i
 
+class (Card size) => DownBoundedCT l size i | l -> size, l -> i where
+--class (Reifies size Int) => UpBoundedCT l size i | l -> size, l -> i where
+
+-- has upper and lower boundaries:
+class BoundedCont l i | l -> i where
+	bounds :: l -> (i,i)
+
+class (Card min, Card max) => BoundedCT l min max i | l -> min, l -> max, l -> i
+
+instance (Card size, UpBoundedCT l size Int) => UpBounded l Int where
+	upperBound x = fromCard (undefined :: size)
+instance (Card size, DownBoundedCT l size Int) => DownBounded l Int where
+	lowerBound x = fromCard (undefined :: size)
+instance (Card min, Card max, BoundedCT l min max Int) => BoundedCont l Int where
+	bounds _ = (lower, upper)
+		where
+			lower = fromCard (undefined :: min)
+			upper = fromCard (undefined :: max)
+
+class FromFunction f x y | f -> x, f -> y where
+	fromFunction :: (x -> y) -> f
+
+class ToFunction f x y | f -> x, f -> y where
+	toFunction :: f -> x -> y
+
+-- infixl 8 |+|
+--a -> a -> a
+--class MatrAdd a where
+-- |+| :: a -> a -> a
+
+mAdd :: forall f i a . (Num a, Ix i, ToFunction f i a, FromFunction f i a) => f -> f -> f
+mAdd l r = fromFunction f
+	where
+		f :: i -> a 
+		f i = (toFunction l) i + (toFunction r) i
+{-
+mAdd :: forall f a . (Num a, ToFunction f Int a, FromFunction f Int a) => f -> f -> f
+mAdd l r = fromFunction f
+	where
+		f :: Int -> a
+		f i = (toFunction l) i + (toFunction r) i
+-}
+
+data ListCTSize a size = ListCTSize { fromTestList :: [a] }
+	deriving( Show )
+
+createTestList :: forall a size . (Card size) => [a] -> Maybe (ListCTSize a size)
+createTestList list = takeSafe (fromCard (undefined :: size)) list >>= return . ListCTSize
+
+{-
+test' i = withInt i func
+
+func :: forall n . (Card n) => n -> Maybe String
+func n = do 
+	list <- (createTestList [1..] :: Maybe (ListCTSize Int n))
+	return $ show $ (list `mAdd` list)
+-}
+
+instance (Card size) => UpBoundedCT (ListCTSize a size) size Int
+instance (Card size) => ToFunction (ListCTSize a size) Int a where
+	toFunction l i = case i < fromCard (undefined :: size) of
+		False -> error "index error"
+		True -> fromTestList l !! i
+instance (Card size) => FromFunction (ListCTSize a size) Int a where
+	fromFunction f = fromJust $ createTestList [ f i | i <- [0..(fromCard (undefined :: size)-1)] ]
+{-
+instance FromFunction (ListCTSize Int N3) Int Int where
+	fromFunction f = fromJust $ createTestList [ f i | i <- [0..(fromCard (undefined :: N3)-1)] ]
+-}
+
+test' :: Maybe (ListCTSize Int N3)
+test' = do
+	l <- (createTestList [1..] :: Maybe (ListCTSize Int N3))
+	return $ mAdd l l
+
+{-
 class DownBounded l i | l -> i where
 	lowerBound :: l -> i
 
@@ -57,6 +124,7 @@ instance (Card min, Card max, BoundedCT l min max Int) => BoundedCont l Int wher
 		where
 			lower = toInt (undefined :: min)
 			upper = toInt (undefined :: max)
+-}
 {-
 class (CardList N2 bounds) => BoundedCT l bounds i | l -> bounds, l -> i
 
@@ -94,11 +162,21 @@ instance Listable (a,a,a,a,a, a) a where toList (a1,a2,a3,a4,a5,a6) = [a1, a2, a
 instance Listable [a] a where
 	toList l = l
 
+{-
 instance ListableCT N2 (a,a) a where
 instance ListableCT N3 (a,a,a) a 
 instance ListableCT N4 (a,a,a,a) a 
 instance ListableCT N5 (a,a,a,a,a) a 
 instance ListableCT N6 (a,a,a,a,a, a) a 
+-}
+
+{-
+instance ListableCT N2 (a,a) a where
+instance ListableCT N3 (a,a,a) a 
+instance ListableCT N4 (a,a,a,a) a 
+instance ListableCT N5 (a,a,a,a,a) a 
+instance ListableCT N6 (a,a,a,a,a, a) a 
+-}
 
 newtype ListFL dim a = ListFL { flToList :: [a] }
 	deriving( Show )
@@ -107,7 +185,7 @@ instance (Card size) => Listable (ListFL size a) a where
 instance (Card size) => ListableCT size (ListFL size a) a
 
 listFLFromList :: Card size => size -> [a] -> Maybe (ListFL size a)
-listFLFromList size list = takeSafe (toInt size) list >>= return . ListFL
+listFLFromList size list = takeSafe (fromCard size) list >>= return . ListFL
 
 takeSafe n list = let l = take n list in
 	if length l == n
